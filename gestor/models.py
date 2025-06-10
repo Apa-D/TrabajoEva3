02ffe508c735adf9a012256f10abaf61f34a7c8d
unknown <diego.ruiz3@virginiogomez.cl>
2025-06-10 11:43:28 -0400

    Modelo y Configurasiones

--- gestor/models.py
+++ gestor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Nota(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+    
+
+class Archivo(models.Model):
+    nombre_archivo = models.CharField(max_length=255)
+    archivo = models.FileField(upload_to='archivos/')
+    fecha_subida = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre_archivo
